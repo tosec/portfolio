77c855107a98067a4e255fbb477ff6f5ddc7ede6
tosec <stephenvallejo44@gmail.com>
2023-07-04 22:56:08 -0400

    addd link to name in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer className="flex justify-center items-center text-xl py-12 bg-neutral-900">
-      Made by tosec with ❤️
+      <div>
+        Made by <span><Link to={"https://github.com/tosec"}>tosec</Link></span> with ❤️
+      </div>
     </footer>
   );
 };
